10692aa230e6835f2f8b953bc6c7e8c983db837a
Alejandro Di Leo <alejandro.dileo@fashion.cloud>
2020-09-17 18:11:49 +0200

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import Nav from '../components/nav'
+import { formatDistanceToNowStrict } from 'date-fns';
 
 export default function IndexPage() {
+  const originDate = new Date(2020, 7, 1);
   return (
-    <div>
-      <Nav />
-      <div className="py-20">
-        <h1 className="text-5xl text-center text-accent-1">
-          Next.js + Tailwind CSS
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="py-20 max-w-6xl">
+        <h1 className="text-6xl text-center text-accent-1">
+          It's been { formatDistanceToNowStrict(originDate, { unit: 'day' }) } since the club-mate has been standing in Ale's desk.
         </h1>
       </div>
     </div>
